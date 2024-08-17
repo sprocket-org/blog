8d3c2533d5e8c33439345a7ba1725eec46ecc4e8
gearsandcode <gearsandcode@gmail.com>
2024-08-17 16:02:03 -0700

    chore: update blog and clear images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,19 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
-    loader: "default",
-    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      }
+    ],
   },
 }
 
